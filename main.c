42c508d9e44767d4502db95bad7de394638020b1
Fakerunner <46268799+Fakerunner@users.noreply.github.com>
2018-12-31 11:51:52 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-int  i ,j ,e, f, g, a, b, d, c;
+int  i ,j ,e, f, g, a, b, d, c, k, L, B;
 void stworz_tablice (void);
 void drukuj_tablice (void);
-void zmien_tablice (void);
 void co_teraz (void);
+void drukuj_wartosci(void);
+void sprawdz_numerek(void);
+void rozlosuj_bomby (void);
 struct daneos
 {
     int wartosc;
     int wyglad;
+    char znak;
 };
 struct daneos pole[100][100];
 
@@ -19,13 +22,14 @@ int main()
     scanf ("%d", &b);
     printf ("Podaj dlugosc");
     scanf ("%d", &a);
+    printf ("Ile bomb ma byæ na mapie?");
+    scanf ("%d", &B);
+    void rozlosuj_bomby (void);
     stworz_tablice();
     drukuj_tablice();
     for (c=0; ;c++)
     {
         co_teraz();
-        zmien_tablice();
-        drukuj_tablice();
     }
     return 0;
 }
@@ -38,6 +42,7 @@ void stworz_tablice(void)
         {
             pole[i][j].wyglad='X';
             pole[i][j].wartosc=5;
+            pole[i][j].znak="B";
         }
     }
 }
@@ -55,10 +60,31 @@ void drukuj_tablice(void)
     printf ("\n");
 }
 
-void zmien_tablice (void)
+void drukuj_wartosci(void)
 {
-    system("cls");
-    pole[4][8].wyglad='G';
+    for (i=0;i<a;i++)
+    {
+        printf ("\n");
+        for (j=0;j<b;j++)
+        {
+            printf ("%c ", pole[i][j].znak);
+        }
+    }
+    printf ("\n");
+}
+
+void sprawdz_numerek(void)
+{
+    L=0;
+    for (i=i-2; i<i+1; i++)
+    {
+       for (j=j-2; j<j+1; j++)
+       {
+            if (pole[i-1][j-1].wartosc==5)
+                L++;
+       }
+    }
+    pole[i-1][j-1].wyglad="L";
 }
 
 void co_teraz(void)
@@ -68,24 +94,20 @@ void co_teraz(void)
     printf ("Dlugosc:");
     scanf ("%d", &j);
     printf ("Co chcesz zrobic?\n1 - odkryc\n2 - oznaczyc jako bombe\n3 - powrot");
-    scanf ("%d", d);
+    scanf ("%d", &d);
     switch (d)
     {
         case 1:
-            if (pole[i][j].wartosc==5)
+            if (pole[i-1][j-1].wartosc==5)
+                system("cls");
+                drukuj_wartosci();
                 printf ("Przegrales");
+                exit (0);
         break;
         case 2:
-            pole[i][j].wyglad='B';
-            for (i=0;i<a;i++)
-            {
-                printf("\n");
-                for (j=0;j<b;j++)
-                {
-                    printf ("%c ", pole[i][j].wyglad);
-                }
-            }
-    printf ("\n");
+            pole[i-1][j-1].wyglad='B';
+            system("cls");
+            drukuj_tablice();
         break;
         case 3:
         break;
